8073db919bc199fd47aa43850b0e44ac8522425f
Shinsuke  May <s.seanmay@gmail.com>
2021-03-25 10:58:59 +0900

    set up pre-commit eslint

---
+++ gatsby-blog/src/pages/__tests__/index.js
@@ -0,0 +1,23 @@
+// import React from 'react'
+// import renderer from 'react-test-renderer'
+// import { render } from '@testing-library/react';
+
+// import IndexPage from "../index"
+
+// const data = [{
+//   allStrapiArticle: {
+//     edges: {
+//       node: {
+//         title: ['Gatsby Default Starter', "Hello"],
+//         content: ["Wow this is amazing content", "so is this"],
+//         id: [0, 1]
+//       }
+//     }
+//   }
+// }]
+
+// describe('Index page', () => {
+//     it('renders correctly', () => {
+//         render(<IndexPage data={data} />);
+//     });
+// });
