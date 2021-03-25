16c2527621616276cb9bfeca4cdcef6827abdea5
Shinsuke  May <s.seanmay@gmail.com>
2021-03-25 18:33:57 +0900

    article test

--- gatsby-blog/src/templates/__tests__/article.js
+++ gatsby-blog/src/templates/__tests__/article.js
@@ -1,10 +1,16 @@
 import React from "react"
 import { render } from "@testing-library/react"
  
-// You have to write data-testid
+
 const ArticleTitle = () => <h1 data-testid="title">Cool blog title</h1>
+
+test("testing test", () => {
+	expect(true).toBe(true)
+})
  
 test("Displays article title", () => {
   const { getByTestId } = render(<ArticleTitle />)  
   expect(getByTestId("title")).toHaveTextContent("Cool blog title")
 })
+
+
