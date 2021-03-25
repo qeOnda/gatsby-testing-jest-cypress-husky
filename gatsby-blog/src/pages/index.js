831600cadb77a48423583218e43a6e69fc05a469
Shinsuke  May <s.seanmay@gmail.com>
2021-03-25 12:49:17 +0900

    pre-commit eslint

--- gatsby-blog/src/pages/index.js
+++ gatsby-blog/src/pages/index.js
@@ -2,26 +2,28 @@ import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <ul>
-      {data.allStrapiArticle.edges.map(document => (
-        <li key={document.node.id}>
-          <h2>
-            <Link to={`/${document.node.id}`}>{document.node.title}</Link>
-          </h2>
-          <p>{document.node.content}</p>
-        </li>
-      ))}
-    </ul>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default function IndexPage({ data }) {
+  return ( 
+    <Layout>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>    
+      <ul>
+        {data.allStrapiArticle.edges.map(document => (
+          <li key={document.node.id}>
+            <h2>
+              <Link to={`/${document.node.id}`}>{document.node.title}</Link>
+            </h2>
+            <p>{document.node.preview}</p>
+          </li>
+        ))}
+      </ul>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
-export default IndexPage
+
+
 
 export const pageQuery = graphql`  
   query IndexQuery {
@@ -30,7 +32,7 @@ export const pageQuery = graphql`
         node {
           id
           title
-          content
+          preview
         }
       }
     }
---
+++ gatsby-blog/src/templates/article.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import Layout from '../components/layout' 
+
+const ArticleTemplate = ({ data }) => (
+  <Layout>
+    <h1>{data.strapiArticle.title}</h1>
+    <Img fluid={data.strapiArticle.image.childImageSharp.fluid} />
+    <p>{data.strapiArticle.content}</p>
+  </Layout>
+)
+
+export default ArticleTemplate
+
+export const query = graphql`
+  query ArticleTemplate($id: String!) {
+    strapiArticle(id: {eq: $id}) {
+      title
+      content
+       image {
+        childImageSharp {
+          fluid(maxWidth: 960) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }      
+    }
+  }
+`
